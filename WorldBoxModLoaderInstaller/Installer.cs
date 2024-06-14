06db9eaec3b040fa019c5adc078d4a59770ab48b
DipCrai <dipcraitop1gg@gmail.com>
2024-06-14 18:19:30 +0300

    added installer

---
+++ WorldBoxModLoaderInstaller/Installer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldBoxModLoaderInstaller
+{
+    public class Installer
+    {
+        [STAThreadAttribute]
+        public static void Main()
+        {
+            var worldboxDirectory = ShowDialog("Select WorldBox directory");
+            var managedDirectory = worldboxDirectory + "\\worldbox_Data\\Managed";
+            var binDirectory = Path.GetFullPath("bin");
+            Console.WriteLine(worldboxDirectory);
+            Console.WriteLine(managedDirectory);
+            Console.WriteLine(binDirectory);
+            if (worldboxDirectory != default)
+            {
+                if (Directory.Exists(binDirectory))
+                {
+                    if (Directory.Exists(managedDirectory))
+                    {
+                        string[] bins = Directory.GetFiles(binDirectory);
+                        foreach (string file in bins)
+                        {
+                            Console.WriteLine(file);
+                            var fileName = Path.GetFileName(file);
+                            var managedFile = managedDirectory + "\\" + fileName;
+                            Console.WriteLine(managedFile);
+                            if (fileName != "Assembly-CSharpTrue.dll")
+                            {
+                                if (fileName != "WorldBoxModLoader.dll" && fileName != "Assembly-CSharp.dll")
+                                {
+                                    if (!File.Exists(managedFile))
+                                        File.Copy(file, managedFile, false);
+                                    else
+                                        continue;
+                                }
+                                else
+                                    File.Copy(file, managedDirectory + "\\" + fileName, true);
+                            }
+                            else
+                                continue;
+                        }
+                        Console.WriteLine("\nWorldBoxModLoader successfully installed!");
+                    }
+                    else
+                        Console.WriteLine("\nSelected directory is not WorldBox directory!");
+                }
+                else
+                    Console.WriteLine("\nError occured during loading files from bin directory, make sure you haven't moved the program to another directory or deleted the bin folder.");
+            }
+            else
+                Console.WriteLine("\nInstallation was cancelled.");
+
+            Console.Write("\nPress any key to exit the program..." );
+            Console.ReadKey();
+        }
+
+        private static string ShowDialog(string description)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = description;
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    return dialog.SelectedPath;
+                }
+                else
+                    return default;
+            }
+        }
+    }
+}
---
+++ WorldBoxModLoaderInstaller/Uninstaller.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldBoxModLoaderInstaller
+{
+    public class Uninstaller
+    {
+        [STAThreadAttribute]
+        public static void Main()
+        {
+            var worldboxDirectory = ShowDialog("Select WorldBox directory");
+            var managedDirectory = worldboxDirectory + "\\worldbox_Data\\Managed";
+            var binDirectory = Path.GetFullPath("bin");
+            Console.WriteLine(worldboxDirectory);
+            Console.WriteLine(managedDirectory);
+            Console.WriteLine(binDirectory);
+            if (worldboxDirectory != default)
+            {
+                if (Directory.Exists(binDirectory))
+                {
+                    if (Directory.Exists(managedDirectory))
+                    {
+                        string[] bins = Directory.GetFiles(binDirectory);
+                        foreach (string file in bins)
+                        {
+                            var fileName = Path.GetFileName(file);
+                            Console.WriteLine(file);
+                            var managedFile = managedDirectory + "\\" + fileName;
+                            Console.WriteLine(managedFile);
+                            if (fileName != "Assembly-CSharpTrue.dll")
+                            {
+                                if (File.Exists(managedFile))
+                                    File.Delete(managedFile);
+                                else
+                                    continue;
+                            }
+                            else
+                            {
+                                var assemblyCsName = Path.GetDirectoryName(managedFile) + "\\Assembly-CSharp.dll";
+                                File.Delete(assemblyCsName);
+                                File.Copy(file, assemblyCsName, true);
+                            }
+                        }
+                        Console.WriteLine("\nWorldBoxModLoader successfully uninstalled!");
+                    }
+                    else
+                        Console.WriteLine("\nSelected directory is not WorldBox directory!");
+                }
+                else
+                    Console.WriteLine("\nError occured during loading files from bin directory, make sure you haven't moved the program to another directory or deleted the bin folder.");
+            }
+            else
+                Console.WriteLine("\nInstallation was cancelled.");
+
+            Console.Write("\nPress any key to exit the program...");
+            Console.ReadKey();
+        }
+
+        private static string ShowDialog(string description)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = description;
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    return dialog.SelectedPath;
+                }
+                else
+                    return default;
+            }
+        }
+    }
+}
